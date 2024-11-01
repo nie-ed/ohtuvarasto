f942db4c325039c4cf4c87a32233f9b07b944d7a
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2024-11-01 12:09:45 +0200

    Pieni testi muutos

--- src/varasto.py
+++ src/varasto.py
@@ -5,7 +5,6 @@ class Varasto:
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-            #muutos
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
